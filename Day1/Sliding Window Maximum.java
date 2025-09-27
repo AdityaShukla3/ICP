ccbe67858bb9a8f99cb2b6a5b871c4dbdb320c11
AdityaShukla3 <adityashukla2036@gmail.com>
2025-09-27 11:12:09 +0530

    Update Sliding Window Maximum.java

--- Day1/Sliding Window Maximum.java
+++ Day1/Sliding Window Maximum.java
@@ -1,11 +1,9 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n=nums.length;
-        if(nums==null ||nums.length==0){
-            return new int[0];
-        }
+        if(nums.length==0) return new int[0];
         int[] arr=new int[n-k+1];
-        for(int i=0;i<=n-k;i++){
+        for(int i=0;i<n-k+1;i++){
             int max=Integer.MIN_VALUE;
             for(int j=i;j<i+k;j++){
                 max=Math.max(max,nums[j]);
