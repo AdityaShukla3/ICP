1eac8d1a922b6ad0d0da1803348bfd11997795bb
AdityaShukla3 <adityashukla2036@gmail.com>
2025-10-03 11:32:34 +0530

    Update Minimum Number of Arrows to Burst Balloons.java

--- Day5/Minimum Number of Arrows to Burst Balloons.java
+++ Day5/Minimum Number of Arrows to Burst Balloons.java
@@ -1,22 +1,16 @@
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        Arrays.sort(points,(a,b) -> Integer.compare(a[0],b[0]));
-        int arrow=1;
-        int pStart=points[0][0];
-        int pEnd=points[0][1];
-        for(int i=1;i<points.length;i++){
-            int curStart=points[i][0];
-            int curEnd=points[i][1];
-            if(curStart>pEnd){
-                arrow++;
-                pStart=curStart;
-                pEnd=curEnd;
-            }
-            else{
-                pStart=Math.max(pStart,curStart);
-                pEnd=Math.min(pEnd,curEnd);
+        int c=1;
+        int n=points.length;
+        Arrays.sort(points,(a,b) -> Integer.compare(a[1],b[1]));
+
+        int end=points[0][1];
+        for(int i=1;i<n;i++){
+            if(points[i][0]>end){
+                c++;
+                end=points[i][1];
             }
         }
-        return arrow;
+        return c;
     }
 }
